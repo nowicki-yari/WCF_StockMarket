c1a077894aaca5d3b51bfe505be5a17d3f6520d4
nowicki-yari <yari.nowicki@outlook.com>
2021-12-04 13:24:10 +0100

    Add project files.

---
+++ WCF_StockMarket/App_Code/Exchange.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Exchange
+/// </summary>
+/// 
+
+namespace NPExchange
+{
+    public class Exchange
+    {
+        private int id;
+        private string name;
+        private string shortName;
+        private string country;
+        private string currency;
+
+
+        public Exchange(int id, string name, string shortName, string country, string currency)
+        {
+            this.id = id;
+            this.name = name;
+            this.shortName = shortName;
+            this.country = country;
+            this.currency = currency;
+        }
+
+        private Exchange()
+        {
+
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string ShortName
+        {
+            get { return shortName; }
+            set { shortName = value; }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+
+        public string Currency
+        {
+            get { return currency; }
+            set { currency = value; }
+        }
+    }
+}
---
+++ WCF_StockMarket/App_Code/Exchanges.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http.Cors;
+using System.Web.Services;
+using System.Data.SqlClient;
+using NPExchange;
+
+/// <summary>
+/// Summary description for Exchanges
+/// </summary>
+[WebService(Namespace = "http://stockmarketviewer.azurewebsites.net/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Exchanges : System.Web.Services.WebService
+{
+
+    public Exchanges()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+    [WebMethod]
+    [EnableCors(origins: "http://stockmarketviewer.azurewebsites.net/", headers:"*", methods:"*")]
+    public List<Exchange> GetExchanges()
+    {
+        Exchange exchange;
+        List<Exchange> exchanges = new List<Exchange>();
+        try
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "stockmarket-cloudcomputing.database.windows.net";
+            builder.UserID = "nowicki-yari";
+            builder.Password = "CLOUD_4090";
+            builder.InitialCatalog = "StockMarketViewer";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+                String sql = "SELECT * FROM dbo.exchanges";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            exchange = new Exchange(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4));
+                            exchanges.Add(exchange);
+                        }
+                        
+                    }
+                }
+            }
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        return exchanges;
+    }
+
+}
